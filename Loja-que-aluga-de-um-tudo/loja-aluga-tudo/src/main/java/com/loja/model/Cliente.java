5952521cb3e9e9f4746ae51aae8ecd297d80018c
joaogt01 <joao.dev06@gmail.com>
2026-06-09 20:55:41 -0300

    feat: adiciona a entidade Usuario e suas subclasses

--- Loja-que-aluga-de-um-tudo/loja-aluga-tudo/src/main/java/com/loja/model/Cliente.java
+++ Loja-que-aluga-de-um-tudo/loja-aluga-tudo/src/main/java/com/loja/model/Cliente.java
@@ -1,4 +1,9 @@
 package com.loja.model;
 
-public class Cliente extends Usuario{
+public class Cliente extends Usuario {
+
+    public Cliente(String nome, String login, String senha) {
+        super(nome, login, senha);
+    }
+
 }
--- Loja-que-aluga-de-um-tudo/loja-aluga-tudo/src/main/java/com/loja/model/Usuario.java
+++ Loja-que-aluga-de-um-tudo/loja-aluga-tudo/src/main/java/com/loja/model/Usuario.java
@@ -1,4 +1,53 @@
 package com.loja.model;
 
+import java.util.UUID;
+
 public abstract class Usuario {
+
+    private final String id;
+    private String nome;
+    private String login;
+    private String senha;
+    private boolean ativo;
+
+    protected Usuario(String nome, String login, String senha) {
+        this.id = UUID.randomUUID().toString();
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        this.ativo = true;
+    }
+
+    public void arquivarUsuario() {
+        this.ativo = false;
+    }
+
+    public void reativarUsuario() {
+        this.ativo = true;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getLogin() {
+        return login;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public boolean estaAtivo() {
+        return ativo;
+    }
+
+    public void atualizarDados(String nome, String login) {
+        this.nome = nome;
+        this.login = login;
+    }
+
+    public void alterarSenha(String novaSenha) {
+        this.senha = novaSenha;
+    }
 }
