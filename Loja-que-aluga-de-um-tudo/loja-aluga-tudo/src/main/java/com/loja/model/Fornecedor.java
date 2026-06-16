d2b9cc29b6cdb1d2bb4fe0a6aba5ad36aa77cc1b
Arthur Dias <arthurdiasm28@gmail.com>
2026-06-15 21:12:04 -0300

    Terminando de implementar a identidade Fornecedor. Implementei o restante dos getteres e os setteres e o método isAtivo

--- Loja-que-aluga-de-um-tudo/loja-aluga-tudo/src/main/java/com/loja/model/Fornecedor.java
+++ Loja-que-aluga-de-um-tudo/loja-aluga-tudo/src/main/java/com/loja/model/Fornecedor.java
@@ -2,25 +2,56 @@ package com.loja.model;
 
 public class Fornecedor {
     private String id;
-    private String razaoSocial;
     private String cnpj;
+    private String telefone;
+    private String nome;
+    private boolean ativo;
 
-    public Fornecedor(String id, String razaoSocial, String cnpj){
+    public Fornecedor(String id, String nome, String cnpj, String telefone) {
         this.id = id;
-        this.razaoSocial = razaoSocial;
+        this.nome = nome;
         this.cnpj = cnpj;
+        this.telefone = telefone;
+        this.ativo = true;
     }
 
     public String getId(){
         return this.id;
     }
 
-    public String getRazaoSocial(){
-        return this.razaoSocial;
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
     }
 
     public String getCnpj(){
         return this.cnpj;
-        
+    }
+
+    public void setCnpj(String cnpj){
+        this.cnpj = cnpj;
+    }
+
+    public String getTelefone(){
+        return this.telefone;
+    }
+
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
     }
 }
