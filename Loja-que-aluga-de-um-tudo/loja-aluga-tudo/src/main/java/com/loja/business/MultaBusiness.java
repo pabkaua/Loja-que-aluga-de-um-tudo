57b93e0041932a80588fb447511fcd49059a25c3
Diego Lyra <diegofernandes.lyra@gmail.com>
2026-06-17 15:44:18 -0300

    feat: adicionando a estrutura inicial de MultaBusiness

--- Loja-que-aluga-de-um-tudo/loja-aluga-tudo/src/main/java/com/loja/business/MultaBusiness.java
+++ Loja-que-aluga-de-um-tudo/loja-aluga-tudo/src/main/java/com/loja/business/MultaBusiness.java
@@ -1,5 +1,36 @@
-package com.loja.services;
+package com.loja.business;
 
-public class MultaService {
-    
+import com.loja.model.Multa;
+import com.loja.model.ContratoAluguel;
+
+
+import java.util.List;
+
+public class MultaBusiness {
+
+    public MultaBusiness(){
+
+    }
+
+    public Multa aplicar(ContratoAluguel contrato){
+
+    }
+
+    public double calcularAtraso(ContratoAluguel contrato){
+
+    }
+
+    public void quitar(String multaId){
+
+    }
+
+    public List<Multa> listarPorCliente(String clienteId){
+
+    }
+
+    public List<Multa> listar(){
+
+    }
+     
 }
+
