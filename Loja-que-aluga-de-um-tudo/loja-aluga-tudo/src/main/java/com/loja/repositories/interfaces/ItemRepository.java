cd74f50cff433e2d5437696cf1defa124a2b49ae
Kauã <kauapablo940@gmail.com>
2026-06-20 11:45:22 -0300

    feat: criando as interfaces ItemRepository e CategoriaRepository

--- Loja-que-aluga-de-um-tudo/loja-aluga-tudo/src/main/java/com/loja/repositories/interfaces/ItemRepository.java
+++ Loja-que-aluga-de-um-tudo/loja-aluga-tudo/src/main/java/com/loja/repositories/interfaces/ItemRepository.java
@@ -1,4 +1,20 @@
 package com.loja.repositories.interfaces;
+import com.loja.model.Item;
+import java.util.Map;
 
 public interface ItemRepository {
+    public void salvar(Item item);
+    public Item buscar(String id);
+
+    // O conjunto chave-valor vai ser String-Objeto, sendo a String o ID para o hashmap
+    public Map<String, Item> listar();
+    public Map<String, Item> listarPorStatus(String status);
+    public Map<String, Item> listarPorCategoria(String categoriaId);
+    public Map<String, Item> listarPorFornecedor(String fornecedorId);
+
+    public void atualizar(Item item);
+    public boolean deletar(String id);
+
+    public void carregarDados();
+    public void salvarDados();
 }
